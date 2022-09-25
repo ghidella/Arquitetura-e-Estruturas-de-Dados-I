83bc1ae812fb1a83f9093a212961cef2106f7c36
Ghidella <raphaelfghidella@gmail.com>
2022-09-25 14:44:54 -0300

    lista ligada dinamica

---
+++ 1. Listas Lineares/Lista Ligada/Implementação Dinâmica/Lista_ligada_dinamica.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef int bool;
+typedef int TIPOCHAVE;
+
+typedef struct {
+    TIPOCHAVE chave;
+} REGISTRO;
+
+typedef struct aux {
+    REGISTRO reg;
+    struct aux* prox;
+} ELEMENTO;
+
+typedef ELEMENTO* PONT;
+
+typedef struct {
+    PONT inicio;
+} LISTA;
+
+int main(){
+
+}
